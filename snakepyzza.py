04ca61915e0a5191c8cb076741aeb1a9854abb67
RenzPy <renzpy@proton.me>
2023-10-26 13:09:21 -0400

    - Fixed window crash error - Added '𝘚𝘯𝘢𝘬𝘦𝘗𝘺𝘻𝘻𝘢 - 𝘳𝘦𝘯𝘻𝘱𝘺 ' window title

--- snakepyzza.py
+++ snakepyzza.py
@@ -11,5 +11,13 @@ import pygame
 pygame.init()
 dis = pygame.display.set_mode((400,300))
 pygame.display.update()
+pygame.display.set_caption('𝘚𝘯𝘢𝘬𝘦𝘗𝘺𝘻𝘻𝘢 - 𝘳𝘦𝘯𝘻𝘱𝘺 ')
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        print(event) #prints out all the actions that take place on the screen
+
+
+    
 pygame.quit
 quit()

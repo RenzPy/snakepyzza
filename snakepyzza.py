af490c7e341eab1be6dde33f132297d27320773b
RenzPy <renzpy@proton.me>
2023-10-26 12:58:14 -0400

    - Added game window

---
+++ snakepyzza.py
@@ -0,0 +1,15 @@
+import pygame 
+
+# SNAKEPYZZA PIZZA EATING SNAKE GAME
+# FOLLOWING TUTORIAL https://www.edureka.co/blog/snake-game-with-pygame/#building
+# README.md FOR MORE INFORMATION
+# RENZPY PROJECT 2023
+
+
+
+# CREATING GAME WINDOW
+pygame.init()
+dis = pygame.display.set_mode((400,300))
+pygame.display.update()
+pygame.quit
+quit()

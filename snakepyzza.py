aff44ea0833b44032fcdfc9f24f865e89892d1c8
RenzPy <renzpy@proton.me>
2023-10-27 05:52:45 -0400

    - Fixed score counter
    Found modules files i created to have a clean project was useless RIP

--- snakepyzza.py
+++ snakepyzza.py
@@ -49,6 +49,56 @@ pizza_position = [random.randrange(1, (window_x//10)) * 10,
                   random.randrange(1, (window_y//10)) * 10]
 pizza_spawn = True
 
+
+# DISPLAY SCORE FUNCTION
+def show_score(choice, color, font, size):
+   
+    # creating font object score_font 
+    score_font = pygame.font.SysFont(font, size)
+     
+    # create the display surface object
+    # score_surface
+    score_surface = score_font.render('PyzzaScore™: ' + str(score), True, color)
+     
+    # create a rectangular object for the 
+    # text surface object
+    score_rect = score_surface.get_rect()
+     
+    # displaying text
+    game_window.blit(score_surface, score_rect)
+
+
+# GAME OVER
+def game_over():
+# creating font object my_font
+    my_font = pygame.font.SysFont('times new roman', 50)
+     
+    # creating a text surface on which text 
+    # will be drawn
+    game_over_surface = my_font.render('PyzzaScore™: ' + str(score), True, red)
+     
+    # create a rectangular object for the text
+    # surface object
+    game_over_rect = game_over_surface.get_rect()
+     
+    # setting position of the text
+    game_over_rect.midtop = (window_x/2, window_y/4)
+     
+    # blit will draw the text on screen
+    game_window.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+     
+    # after 2 seconds we will quit the 
+    # program
+    
+     
+    # deactivating pygame library
+    pygame.quit()
+     
+    # quit the program
+    quit()
+
+
 # Main Function
 running = True
 while running:
@@ -115,17 +165,17 @@ while running:
  
     # Game Over conditions
     if snake_position[0] < 0 or snake_position[0] > window_x-10:
-        skpy.game_over()
+        game_over()
     if snake_position[1] < 0 or snake_position[1] > window_y-10:
-        skpy.game_over()
+        game_over()
      
     # Touching the snake body
     for block in snake_body[1:]:
         if snake_position[0] == block[0] and snake_position[1] == block[1]:
-            skpy.game_over()
+            game_over()
      
     # displaying score continuously
-    skpy.show_score(1, white, 'times new roman', 20)
+    show_score(1, white, 'times new roman', 20)
      
     # Refresh game screen
     pygame.display.update()

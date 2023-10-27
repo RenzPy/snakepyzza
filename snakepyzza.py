2e92cbc02e9486ad58f7e9767f336de4e507d889
RenzPy <renzpy@proton.me>
2023-10-27 02:50:36 -0400

    - Complete revamp - Tutorial change - Added game colors and FPS controller

--- snakepyzza.py
+++ snakepyzza.py
@@ -1,23 +1,32 @@
-import pygame 
+# IMPORTING LIBRARIES
+import pygame
+import time
+import random
 
 # SNAKEPYZZA PIZZA EATING SNAKE GAME
-# FOLLOWING TUTORIAL https://www.edureka.co/blog/snake-game-with-pygame/#building
+# FOLLOWING TUTORIAL https://www.geeksforgeeks.org/snake-game-in-python-using-pygame-module/
 # README.md FOR MORE INFORMATION
 # RENZPY PROJECT 2023
 
+snake_speed = 15
 
+# WINDOW SIZE
+window_x = 720
+window_y = 480
 
-# CREATING GAME WINDOW
+# DEFINING COLORS
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+# INITIALISE GAME
 pygame.init()
-dis = pygame.display.set_mode((400,300))
-pygame.display.update()
-pygame.display.set_caption('𝘚𝘯𝘢𝘬𝘦𝘗𝘺𝘻𝘻𝘢 - 𝘳𝘦𝘯𝘻𝘱𝘺 ')
-game_over = False
-while not game_over:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game_over=True 
 
-    
-pygame.quit
-quit()
+# INITIALISE GAME WINDOW
+pygame.display.set_caption("𝘚𝘯𝘢𝘬𝘦𝘗𝘺𝘻𝘻𝘢 - 𝘳𝘦𝘯𝘻𝘱𝘺") # window title
+game_window = pygame.display.set_mode((window_x,window_y))
+
+# FPS controller
+fps = pygame.time.Clock()
